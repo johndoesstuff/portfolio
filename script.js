73f3dcf058e6ef7c18cc7626cbef428fc50327a0
johndoesstuff <johndoesstuff03@gmail.com>
2025-01-27 19:08:31 -0500

    blehhh :P

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("meow");
+let background = document.getElementById("background");
+
+window.backgroundArray = null;
+
+function getBgSize() {
+	let screenWidth = window.innerWidth;
+	let screenHeight = window.innerHeight;
+	let charElement = document.createElement("div");
+	charElement.innerText = ("M".repeat(100) + "\n").repeat(100);
+	charElement.style.visibility = 'hidden';
+	charElement.id = "charTester";
+	charElement.style.whiteSpace = 'nowrap';
+	charElement.style.position = 'absolute';
+	charElement.style.fontFamily = 'monospace';
+	charElement.style.margin = "0";
+	charElement.style.padding = "0";
+	charElement.style.border = "none";
+
+	document.body.appendChild(charElement);
+
+	const charWidth = charElement.offsetWidth/100;
+	const charHeight = charElement.offsetHeight/100;
+
+	const rows = Math.floor(screenHeight / charHeight);
+	const cols = Math.floor(screenWidth / charWidth);
+
+	document.body.removeChild(charElement);
+
+	return { cols, rows };
+}
+
+function initializeBackground() {
+	let bgSize = getBgSize();
+	let col = bgSize.cols;
+	let row = bgSize.rows;
+
+	window.backgroundArray = JSON.parse(JSON.stringify(Array(row).fill(Array(col).fill("#"))));
+
+	background.innerText = window.backgroundArray.map(e => e.join("")).join("\n");
+}
+
+initializeBackground();
