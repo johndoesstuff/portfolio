942d017204688739622bdba8d8db28156d606c55
johndoesstuff <johndoesstuff03@gmail.com>
2025-02-04 00:02:18 -0500

    back button & minor fixes

--- projects/script.js
+++ projects/script.js
@@ -1,24 +1,24 @@
 let projects = { //object so its easier to change order if needed
 	0: {
-		img: "projects/ebnf.png",
+		img: "ebnf.png",
 		title: "EBNF Parser Generator",
 		description: "A TypeScript tool that parses EBNF syntax and generates a TypeScript program capable of building an abstract syntax tree (AST) for a given input. It automates the creation of parsers from formal grammar definitions, making it useful for language design, interpreters, and compilers.",
 		link: "https://github.com/johndoesstuff/ebnf-parser",
 	},
 	1: {
-		img: "projects/x86raymarcher.png",
+		img: "x86raymarcher.png",
 		title: "86x Assembly Raymarcher",
 		description: "A simple raymarching engine written in x86 assembly, designed as a learning exercise in low-level programming. It renders basic 3D scenes using signed distance functions (SDFs) with a focus on minimalism and performance.",
 		link: "https://github.com/johndoesstuff/asmRaymarcher",
 	},
 	2: {
-		img: "projects/bas.png",
+		img: "bas.png",
 		title: "Tiny BASIC Interpreter",
 		description: "A Tiny BASIC interpreter written in JavaScript that reads and executes programs from a file. It supports variables, arithmetic expressions, and basic control flow, demonstrating efficient parsing and execution of a minimal programming language. It follows the specification outlined in Dr. Dobb's Journal Vol. 1, no. 1. p. 9",
 		link: "https://github.com/johndoesstuff/tinyBasicInterpreter",
 	},
 	3: {
-		img: "projects/wte.png",
+		img: "wte.png",
 		title: "WTE Text Editor",
 		description: "A lightweight text editor built in C, designed for simplicity and performance. It features essential text manipulation tools such as basic editing, saving, and opening files, as well as support for handling user input and text navigation. The editor uses standard console functionality to provide a fast, responsive environment ideal for quick coding sessions and text editing",
 		link: "https://github.com/johndoesstuff/WTE-Text-Editor",
