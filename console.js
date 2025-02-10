af3d6d82f428234ad3ac04b941647e7f08d83960
johndoesstuff <johndoesstuff03@gmail.com>
2025-02-10 18:38:30 -0500

    silly console message

---
+++ console.js
@@ -0,0 +1,5 @@
+console.info(
+	`%c👁️ I see you! 👁️%c \nIf youd like to see how this website was made you can find the source at https://github.com/johndoesstuff/portfolio\n\nOtherwise feel free to poke around!`,
+	"color: #fffdeb; background: #00006b33; font-size: 1.5rem; padding: 0.15rem; margin: 1rem auto; font-family: Rockwell, Tahoma, 'Trebuchet MS', Helvetica; border: 2px solid #fffdeb; border-radius: 4px; font-weight: bold; text-shadow: 1px 1px 1px #000000bf;",
+	'font-weight: bold; font-size: 1rem; color: #fffdeb;'
+);
